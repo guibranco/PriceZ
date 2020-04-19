544e3e39b61377d46294236f59be192148cd65cc
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2020-04-18 21:02:36 -0300

    Add five methods to client

--- Src/PriceZ/IPriceZClient.cs
+++ Src/PriceZ/IPriceZClient.cs
@@ -2,6 +2,8 @@
 {
     using Models;
     using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     /// <summary>
     /// The PriceZ client interface
@@ -9,36 +11,41 @@
     internal interface IPriceZClient
     {
         /// <summary>
-        /// Gets the dd ds.
+        /// Gets the area codes asynchronous.
         /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns></returns>
-        IEnumerable<int> GetDDDs();
+        Task<IEnumerable<int>> GetAreaCodesAsync(CancellationToken cancellationToken);
 
         /// <summary>
-        /// Gets the cities.
+        /// Gets the cities asynchronous.
         /// </summary>
-        /// <param name="ddd">The DDD.</param>
+        /// <param name="areaCode">The area code.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns></returns>
-        IEnumerable<CityData> GetCities(int ddd);
+        Task<IEnumerable<CityData>> GetCitiesAsync(int areaCode, CancellationToken cancellationToken);
 
         /// <summary>
-        /// Gets the states.
+        /// Gets the states asynchronous.
         /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns></returns>
-        IEnumerable<string> GetStates();
+        Task<IEnumerable<string>> GetStatesAsync(CancellationToken cancellationToken);
 
         /// <summary>
-        /// Gets the cities.
+        /// Gets the cities asynchronous.
         /// </summary>
         /// <param name="state">The state.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns></returns>
-        IEnumerable<CityData> GetCities(string state);
+        Task<IEnumerable<CityData>> GetCitiesAsync(string state, CancellationToken cancellationToken);
 
         /// <summary>
-        /// Gets the zipcode.
+        /// Gets the zip code asynchronous.
         /// </summary>
         /// <param name="zipCode">The zip code.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns></returns>
-        ZipCodeData GetZipcode(string zipCode);
+        Task<ZipCodeData> GetZipCodeAsync(string zipCode, CancellationToken cancellationToken);
     }
 }
---
+++ Tests/PriceZ.Tests/PriceZClientTests.cs
@@ -0,0 +1,78 @@
+namespace PriceZ.Tests
+{
+    using System;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class PriceZClientTests
+    {
+        private readonly PriceZClient _priceZ;
+
+        public PriceZClientTests()
+        {
+            var client = new HttpClient { BaseAddress = new Uri("https://ddd.pricez.com.br/") };
+            client.DefaultRequestHeaders.ExpectContinue = false;
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(@"application/json"));
+            _priceZ = new PriceZClient(client);
+        }
+
+        [Fact]
+        public async Task ValidateGetAreCodes()
+        {
+            var areaCodes = await _priceZ.GetAreaCodesAsync(CancellationToken.None);
+            var areaCodesList = areaCodes.ToList();
+            Assert.Contains(11, areaCodesList);
+            Assert.Contains(21, areaCodesList);
+        }
+
+        [Fact]
+        public async Task ValidateGetCitiesByAreaCode()
+        {
+            var cities = await _priceZ.GetCitiesAsync(11, CancellationToken.None);
+            var citiesList = cities.ToList();
+            Assert.Contains(citiesList, city => city.AreaCode.Equals(11));
+            Assert.Contains(citiesList, city => city.City.Equals("São Paulo"));
+            Assert.Contains(citiesList, city => city.StateInitials.Equals("SP"));
+        }
+
+        [Fact]
+        public async Task ValidateGetStates()
+        {
+            var states = await _priceZ.GetStatesAsync(CancellationToken.None);
+            var statesList = states.ToList();
+            Assert.Contains("SP", statesList);
+            Assert.Contains("RJ", statesList);
+        }
+
+        [Fact]
+        public async Task ValidateGetCitiesByState()
+        {
+            var cities = await _priceZ.GetCitiesAsync("SP", CancellationToken.None);
+            var citiesList = cities.ToList();
+            Assert.Contains(citiesList, city => city.AreaCode.Equals(11));
+            Assert.Contains(citiesList, city => city.City.Equals("São Paulo"));
+            Assert.Contains(citiesList, city => city.StateInitials.Equals("SP"));
+        }
+
+        [Fact]
+        public async Task GetZipCode()
+        {
+            var zipCode = await _priceZ.GetZipCodeAsync("01001000", CancellationToken.None);
+            Assert.Equal("Sé", zipCode.Neighborhood);
+            Assert.Equal("01001000", zipCode.ZipCode);
+            Assert.Equal("São Paulo", zipCode.City);
+            Assert.Equal("SP", zipCode.StateInitials);
+            Assert.Equal("Praça da Sé", zipCode.Address);
+            Assert.Equal(11, zipCode.AreaCode);
+            Assert.Equal((decimal)1521.11, zipCode.CityInfo.Area);
+            Assert.Equal("1521.11", zipCode.CityInfo.AreaInternal);
+            Assert.Equal(3550308, zipCode.CityInfo.IBGECode);
+            Assert.Null(zipCode.CityInfo.Name);
+        }
+    }
+}
